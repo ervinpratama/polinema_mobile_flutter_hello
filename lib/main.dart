10ef05c57bd42ff86e8b38664928cbdbd50a5b84
vinsifa <vinpratama12@gmail.com>
2021-10-04 12:13:00 +0700

    hello_world

--- lib/main.dart
+++ lib/main.dart
@@ -99,17 +99,11 @@ class _MyHomePageState extends State<MyHomePage> {
               'Ervin Pratama Sifa Ramadhan',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              '1941720111',
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
